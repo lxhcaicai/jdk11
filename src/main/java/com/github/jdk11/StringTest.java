d8e62bbcc9cada1c35d4eb16bb29fe86834c5c7c
lxhcaicai <2778763221@qq.com>
2023-09-30 18:44:42 +0800

    [JDK11] String 新增处理方法

--- src/main/java/com/github/jdk11/StringTest.java
+++ src/main/java/com/github/jdk11/StringTest.java
@@ -25,4 +25,56 @@ public class StringTest {
         System.out.println(str.isBlank()); //true
         System.out.println(str.isEmpty()); //false
     }
+
+    /**
+     * 去除收尾空白
+     *
+     * 去除收尾空白：包括全角空白字符。
+     * public String strip() {}
+     *
+     * 去除收尾空白：不包括全角空白字符。
+     * public String trim() {}
+     *
+     */
+    @Test
+    public void test2() {
+        String str = "　abc　"; // 有全角空白
+        System.out.println("str.trim() =" + str.trim()); //  str.trim() =　abc
+        System.out.println("str.strip() =" + str.strip()); // str.strip() =abc
+    }
+
+    /**
+     * 去除尾部空白或首部空白
+     *
+     * 去除尾部空白：
+     * public String stripTrailing() {}
+     *
+     * 去除首部空白
+     * public String stripLeading() {}
+     */
+    @Test
+    public void test3() {
+        System.out.println("----" + "  JavaScript   ".stripLeading() + "----"); // ----JavaScript  ----
+        System.out.println("----" + "  JavaScript   ".stripTrailing() + "----"); // ----  JavaScript----
+
+    }
+
+    /**
+     * 复制字符串
+     * public String repeat(int count) {}
+     *
+     */
+    @Test
+    public void test4() {
+        System.out.println("java".repeat(3)); // javajavajava
+    }
+
+    /**
+     * 行数统计：
+     * public Stream<String> lines() {}
+     */
+    @Test
+    public void test5() {
+        System.out.println("你好啊~\n我很好~\n你呢？\n当然，不错。\n幸好...".lines().count()); // 5
+    }
 }
